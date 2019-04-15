de058b698823b7566a1813f3fc542a11d8eb16b1
michaelborisov <michaelborisovha@gmail.com>
2019-04-15 22:29:32 +0200

    Added presenter config to unify behaviour of presenters

---
+++ app/src/main/java/michaelborisov/starwarschallenge/utils/PresenterConfig.kt
@@ -0,0 +1,10 @@
+package michaelborisov.starwarschallenge.utils
+
+import javax.inject.Inject
+
+class PresenterConfig @Inject constructor() {
+
+    val clickDebounce = 300L
+
+    val textInputChangeDebounce = 400L
+}

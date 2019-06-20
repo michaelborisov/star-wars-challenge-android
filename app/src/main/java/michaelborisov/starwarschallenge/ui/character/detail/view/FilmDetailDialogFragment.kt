8246218fa5f6ee44959195d0d57e2f58770e2cfe
michaelborisov <michaelborisovha@gmail.com>
2019-06-20 16:59:55 +0200

    Fix FilmDetail fragment issue at rotation

--- app/src/main/java/michaelborisov/starwarschallenge/ui/character/detail/view/FilmDetailDialogFragment.kt
+++ app/src/main/java/michaelborisov/starwarschallenge/ui/character/detail/view/FilmDetailDialogFragment.kt
@@ -6,8 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import michaelborisov.starwarschallenge.datamodel.Film
 import michaelborisov.starwarschallenge.R
+import michaelborisov.starwarschallenge.datamodel.Film
+
 
 /**
  * DialogFragment to display full information about film.
@@ -20,7 +21,14 @@ class FilmDetailDialogFragment : DialogFragment() {
     private lateinit var filmDate: TextView
     private lateinit var openingCrawl: TextView
 
+    private val filmTitleKey = "filmTitle"
+    private val filmDateKey = "filmDate"
+    private val openingCrawlKey = "openingCrawl"
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        if (savedInstanceState != null) {
+            film = loadFilmInfoFromBundle(savedInstanceState)
+        }
         return inflater.inflate(R.layout.character_detail_film_dialog, container, false)
     }
 
@@ -30,10 +38,32 @@ class FilmDetailDialogFragment : DialogFragment() {
         filmDate = view.findViewById(R.id.tvFilmDate)
         openingCrawl = view.findViewById(R.id.tvOpeningCrawl)
 
-        filmTitle.text = film.title
-        filmDate.text = film.release_date
-        openingCrawl.text = film.opening_crawl
+        if (::film.isInitialized) {
+            filmTitle.text = film.title
+            filmDate.text = film.release_date
+            openingCrawl.text = film.opening_crawl
+        }
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        saveFilmInfoIntoBundle(film, outState)
+    }
+
+    private fun saveFilmInfoIntoBundle(film: Film, outState: Bundle) {
+        outState.putString(filmTitleKey, film.title)
+        outState.putString(filmDateKey, film.release_date)
+        outState.putString(openingCrawlKey, film.opening_crawl)
+    }
+
+    private fun loadFilmInfoFromBundle(savedInstanceState: Bundle): Film {
+        film = Film(
+            savedInstanceState.getString(filmTitleKey),
+            savedInstanceState.getString(filmDateKey),
+            savedInstanceState.getString(openingCrawlKey)
+        )
+        return film
     }
 
     companion object {
